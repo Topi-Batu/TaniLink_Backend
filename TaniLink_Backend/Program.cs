d081f9e4a3a587c9821b385e50f966b3929c4436
hubedid <hubed@protonmail.com>
2023-12-04 22:43:17 +0700

    [ADD] Authentication setting

--- TaniLink_Backend/Program.cs
+++ TaniLink_Backend/Program.cs
@@ -1,16 +1,105 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TaniLink_Backend;
 using TaniLink_Backend.Controllers.GrpcServices;
 using TaniLink_Backend.Data;
+using TaniLink_Backend.Interfaces;
+using TaniLink_Backend.Models;
+using TaniLink_Backend.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http1);
+    options.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddGrpc();
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddTransient<Seed>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineConnection"));
 });
+
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedEmail = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+    options.Lockout.MaxFailedAccessAttempts = 3;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value!))
+        };
+        /*options.Events = new JwtBearerEvents
+        {
+            OnChallenge = async context =>
+            {
+                context.HandleResponse();
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                context.Response.ContentType = "application/json";
+                var result = new UnauthorizedObjectResult(new ResponseHelper().Error("You are not authorized", 401));
+                await result.ExecuteResultAsync(new ActionContext
+                {
+                    HttpContext = context.HttpContext
+                });
+            },
+            OnForbidden = async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                context.Response.ContentType = "application/json";
+                var result = new ObjectResult(new ResponseHelper().Error("You are forbidden to access this resource", 403))
+                {
+                    StatusCode = (int)HttpStatusCode.Forbidden
+                };
+                await result.ExecuteResultAsync(new ActionContext
+                {
+                    HttpContext = context.HttpContext
+                });
+            }
+        };*/
+    });
+
 var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+
+async void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        await service!.SeedDataContextAsync();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -28,4 +117,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+await app.RunAsync();
