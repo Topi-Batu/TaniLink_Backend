d709c133d5a6d04f513014b13ce07ca17bc87bb9
hubedid <hubed@protonmail.com>
2023-12-04 22:42:09 +0700

    [ADD] User model migration

--- TaniLink_Backend/Models/User.cs
+++ TaniLink_Backend/Models/User.cs
@@ -4,6 +4,7 @@ namespace TaniLink_Backend.Models
 {
     public class User : IdentityUser
     {
+        public string? FullName { get; set; }
         public string? Picture { get; set; }
         public ICollection<Area> Areas { get; set; }
     }
