66a0ad0d030d67e3d714db856f424659dc62d82a
hubedid <hubed@protonmail.com>
2023-12-04 22:40:49 +0700

    [ADD] Role seeder

---
+++ TaniLink_Backend/Seed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaniLink_Backend
+{
+    public class Seed
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public Seed(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task SeedDataContextAsync()
+        {
+            if (!await _roleManager.RoleExistsAsync("Admin"))
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                await _roleManager.CreateAsync(new IdentityRole("Mitra"));
+                await _roleManager.CreateAsync(new IdentityRole("Petani"));
+                await _roleManager.CreateAsync(new IdentityRole("User"));
+            }
+            else
+                Console.WriteLine("Already seeded");
+        }
+    }
+}
